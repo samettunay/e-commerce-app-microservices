2ea940e201e57595d74a2fd346c1a853656a4e28
samettunay <samet.tunay12@gmail.com>
2025-03-14 01:35:09 +0300

    added EnableJpaAuditing in PaymentApplication

--- services/payment/src/main/java/com/samet/ecommerce/PaymentApplication.java
+++ services/payment/src/main/java/com/samet/ecommerce/PaymentApplication.java
@@ -2,6 +2,7 @@ package com.samet.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
